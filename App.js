116d0334ae244e91ceecfe867b8c815e1a23aba0
Avey <romeo.pruvost3007@gmail.com>
2021-06-23 18:42:36 +0200

    master

--- App.js
+++ App.js
@@ -1,12 +1,46 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import { StyleSheet, Text, View, TextInput, Button, ScrollView } from 'react-native';
+import { AntDesign } from '@expo/vector-icons'; 
+
 
 export default function App() {
+  let books = [
+    {title: 'Dune', description: 'Un livre avec beaucoup de sable'},
+    {title: 'Mer', description: "Un livre avec beaucoup d'eau"},
+    {title: 'Forêt', description:"C'est boisé par ici"},
+    {title: 'Montagne', description:"C'est un peu haut là.."},
+    {title: 'Marais', description:"Quelque chose m'a touché le pied"},
+    {title: 'Savane', description:'Graouh, dit-alors le lion'},
+    {title: 'Grotte', description:"Bon t'as compris l'truc"}
+  ];
+  const [booksFiltered, setBooksFiltered] = useState([...books]);
+  const [content, setContent] = useState('');
+  const booksFilteredJSX = booksFiltered.map(book => {
+    return (
+      <View>
+      <Text  style={styles.books}>
+        <AntDesign style={styles.icon} name="book" size={24} color="white" /> {book.title}
+      </Text>
+      <Text  style={styles.desc}>
+        {book.description}
+      </Text>
+      </View>
+  )})
+
+  const handleSubmit = () => {
+    setBooksFiltered(books.filter(book => {
+      return book.title.toLowerCase().includes(content.toLowerCase())
+    }))
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.list}>Ma bibliothèque</Text>
+      <View style={styles.form}>
+      <TextInput style={styles.searchBar} value={content} onChangeText={(text) => {setContent(text)}} placeholder="Rechercher.." />
+      <Button title='Go' onPress={handleSubmit}/>
+      </View>
+      {booksFilteredJSX}
     </View>
   );
 }
@@ -14,8 +48,41 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'black',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  list: {
+    fontWeight: 'bold',
+    fontSize: 25,
+    textAlign: 'center',
+    color: 'white'
+  },
+  searchBar: {
+    width: 200,
+    borderColor: 'white',
+    borderWidth: 2,
+    padding: 5,
+    backgroundColor: 'white'
+  },
+  form: {
+    margin: 30,
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+  },
+  books: {
+    flexDirection: 'row',
+    margin: 10,
+    width: 250,
+    borderBottomWidth: 3,
+    borderColor: 'white',
+    color: 'white'
+  },
+  icon: {
+    marginRight: 5
+  },
+  desc: {
+    textAlign: 'center',
+    color: 'white'
+  }
 });
